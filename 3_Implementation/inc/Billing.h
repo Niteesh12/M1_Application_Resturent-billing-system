7aaa5f1def16c274d1ba714e8ee0fcdf2f1cc8c8
Niteeshkumar Reddy Yarabaka <49491210+Niteesh12@users.noreply.github.com>
2021-11-22 01:51:43 +0530

    Add files via upload

---
+++ 3_Implementation/inc/Billing.h
@@ -0,0 +1,15 @@
+/**
+ * @file Billing.h
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-11-22
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#ifndef __Billing__H
+#define __Billing__H
+int qty();
+int numOfItems;
+end if;
